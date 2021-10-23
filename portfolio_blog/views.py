5b1bc02aff1b45b2b83859dea5f69c96a65871d6
Ralph Souza <ralph_souza@hotmail.com>
2021-10-23 15:53:37 -0300

    Add objects filter by publisehd_date to blog views

--- portfolio_blog/views.py
+++ portfolio_blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import PostArticle
 
 
 def post_article(request):
-    return render(request, 'portfolio_blog/post_article.html', {})
+    post_articles = PostArticle.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'portfolio_blog/post_article.html', {'post_articles': post_articles})
